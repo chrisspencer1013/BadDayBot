03e96d108cd9e203ac991650e5ffa9e1e2c0b52c
chrisspencer1013 <chrisspencer1013@gmail.com>
2018-05-30 19:15:54 -0400

    Starting with replies on a by user basis, timeframes, establish tweets already replied to. Rough stuff.

--- BadDayBot.py
+++ BadDayBot.py
@@ -1,11 +1,14 @@
 import tweepy
 from textblob import TextBlob
 import random
-import keys
-import links
-import time
+import datetime as dt
+
+from keys import *
+from links import *
 
 tweetsThisSession = 0
+#currentTime = strftime("%Y-%m-%d %H:%M:%S",gmtime())
+#todo bad day bot welcome splash
 try:
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -13,24 +16,41 @@ try:
 except:
     print("Error: Authentication Failure")
 
-while true:
-    user = api.me()
+user = api.me()
+while True:
+    now = dt.datetime.now()-dt.timedelta(minutes=15)
+    for follower in tweepy.Cursor(api.followers).items():
+        follower.follow() #follow everyone that is following the bot
     for friend in user.friends():
         print(friend.screen_name)
+        tweets = api.user_timeline(friend.screen_name,count=10)
+        for tweet in tweets:
+            if now < tweet.created_at:
+                print('\n\tSkipping old tweets')
+                break
+            with open('replied.txt') as replied:#needs some work
+                for id_string in replied:
+                    if id_string = tweet.id_str:
+                        break
 
-    tweets = api.home_timeline()
-    for tweet in tweets:
-        results = TextBlob(tweet.text)
-        print("\n")
-        print(tweet.text)
-        print(results.sentiment.polarity)
-        if (results.sentiment.polarity < -0.5 and tweetsThisSession < 3):
-            imageLink = CuteAnimalLink()
-            #api.update_status("Bad day? Here's a cute animal! "+ imageLink +" (if you are not having a bad day, please reply to this comment so I can fix the bot)",tweet.id_str)
-            tweetsThisSession+=1
-            print("TWEET OUT HERE!")
-    time.sleep(10)
-    break
+
+
+
+            if "RT " in tweet.text:
+                print("\n\tSkipping RT")
+                break 
+            results = TextBlob(tweet.text)
+            print("\n\t"+str(tweet.created_at))
+            print("\t"+tweet.text)
+            print("\t"+str(results.sentiment.polarity))
+            if (results.sentiment.polarity < -0.5 and tweetsThisSession < 3):
+                imageLink = CuteAnimalLink()
+                #api.update_status("Bad day? Here's a cute animal! "+ imageLink +" (if you are not having a bad day, please reply to this comment so I can fix the bot)",tweet.id_str)
+                tweetsThisSession+=1
+                print("\t\tTWEET OUT HERE! " + CuteAnimalLink())
+        print("____________\n")
+    #time.sleep(10)
+    break#for testing
 
    
 
--- links.py
+++ links.py
@@ -1,3 +1,4 @@
+import random
 def CuteAnimalLink():
     links = ("https://i.imgur.com/mY70ttU.gifv",
     		 "https://i.redd.it/d6sk0ogt3kj01.jpg",
