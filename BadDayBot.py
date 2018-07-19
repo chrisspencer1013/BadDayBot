7004f35308944034aa4fc6f2cc542cadff240360
chriss <chriss@rda.local>
2018-07-19 10:56:24 -0400

    using txt files instead of py, put in (untested) logic for blacklist

--- BadDayBot.py
+++ BadDayBot.py
@@ -4,11 +4,9 @@ import random
 import datetime as dt
 
 from keys import *
-from links import *
 
-tweets_this_session = 0
 #currentTime = strftime("%Y-%m-%d %H:%M:%S",gmtime())
-print("BAD DAY BOT! LET'S MAKE SOMEONES DAY A LITTLE BETTER!")#or at least post a picture to twitter lul
+print("BAD DAY BOT! LET'S MAKE SOMEONE'S DAY A LITTLE BETTER!\n")#or at least post a picture to twitter lul
 try:
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -20,13 +18,17 @@ user = api.me()
 
 for follower in tweepy.Cursor(api.followers).items():
     follower.follow() #follow everyone that is following the bot
-    
-#TODO: unfriend peeps on the blacklist
+
+#not yet tested cause I'm lazy
+with open('blacklist.txt') as b: 
+    blacklist = b.readlines()
+    for user_id in blacklist:
+        user.destroy_friendship(user_id)
 
 friend_list = []
-print("Current allowed users:")
+print("Currently followed users:")
 for friend in user.friends():
-    print(friend.screen_name)
+    print("\t"+friend.screen_name)
     friend_list.append(friend.id_str)
     
 #print(friend_list)
@@ -38,18 +40,22 @@ class TweetListener(tweepy.StreamListener):
             results = TextBlob(status.text)
             print("\t"+str(results.sentiment.polarity))
             if (results.sentiment.polarity < -0.5):
-                image_link = CuteAnimalLink()
-                text_out = "@"+status.user.screen_name+" Bad day? Here's a cute animal! "+ image_link
+                text_out = createMessage(status.user.screen_name)
                 print(text_out)
                 api.update_status(text_out,status.id_str)
-                tweets_this_session+=1
             print("____________\n")
     def on_error(self, status_code):
         if status_code == 420:
-            #returning False in on_data disconnects the stream
             return False
 
+def createMessage(screen_name):
+    with open('messages.txt') as f:
+        message_templates = f.readlines()
+    with open('links.txt') as f:
+        links = f.readlines()
+    return("@"+screen_name+" "+random.choice(message_templates)+" "+ random.choice(links))
 
+print("\nLet's go!")
 tweetListenerInstance = TweetListener()
 tweetStream = tweepy.Stream(auth = api.auth, listener=TweetListener())
 tweetStream.filter(follow=friend_list)
--- links.py
+++
@@ -1,6 +0,0 @@
-import random
-def CuteAnimalLink():
-    links = ("https://i.imgur.com/mY70ttU.gifv",
-    		 "https://i.redd.it/d6sk0ogt3kj01.jpg",
-    		 "https://i.imgur.com/nSis2Hz.gifv")
-    return random.choice(links)
